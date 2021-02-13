483125cdcd81548627b57d4fecd685b4eeba8012
subhampreet <subhampreet18@gmail.com>
2021-02-13 20:21:47 +0530

    Event + Messenger

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from "./component/Header";
 import Profile from "./component/Profile";
+import Messenger from "./component/Messenger";
+import Event from "./component/Event";
 
 function App() {
   return (
     <div className="App">
        <Header />
        <Profile />
+       <Messenger />
+       <Event />
     </div>
   );
 }
